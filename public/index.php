0a4e873d15caf798c2ecad8434150549ae1d60ce
LUIZ FELIPE GASPAR FERNANDES DA SILVA <273435@unifio.edu.br>
2025-09-19 13:18:34 +0000

    att_19-09

--- public/index.php
+++ public/index.php
@@ -1,17 +1,28 @@
-<?php
-require '../vendor/autoload.php';
-
-use Application\Hello;
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <link href="/css/default.css" rel="stylesheet" />
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/normalize.css">
+    <title>Larikas Burguer</title>
 </head>
 <body>
-    <h1>PHP & Mysql</h1>
-    <p><?= Hello::message() ?></p>
+    <header>
+        <div id="logo">
+            <img src="./img/ham-ico-64.png">
+            <p>Larikas Burguer</p>
+        </div>
+        <nav>
+
+        </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
 </body>
 </html>
